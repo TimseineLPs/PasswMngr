7363f1407a8ae2a31680da9545b2fdedf6cb57e7
timseinelps <tim11081999@gmail.com>
2015-11-19 19:43:10 +0100

    started work on backbone

---
+++ backbone.py
@@ -0,0 +1,88 @@
+import random , os , math , binascii , base64 , getpass
+from tkinter import Tk
+from Crypto.Cipher import AES
+
+#https://code.activestate.com/recipes/410692/
+class switch(object):
+	def __init__(self , value):
+		self.value = value
+		self.fall = False
+	def __iter__(self):
+		"""Retrun the match method once, then stop"""
+		yield self.match
+		raise StopIteration
+	def match(self, *args):
+		"""Indicate whether or not to enter a case suite"""
+		if self.fall or not args:
+			return True
+		else:
+			return False
+#usage:
+# for case in switch(x):
+# if case(value):
+# if case() -< returns if no other value matches
+
+class DataModule:
+	def __init__(self, name, password, website):
+		self.website = website
+		self.password = password
+		self.name = name
+		
+def encrypt(data):
+	encoded = EncodeAES(cipher,data)
+	return encoded
+def decrypt(data):
+	decrypt = DecodeAES(cipher,data)
+	return decoded
+	
+def loadPwFile():
+	if os.stat("pws.encr").st_size == 0:
+		return 0
+	else:
+		s = open("pws.encr","rt")
+		dt = s.readline()
+		dt = decrypt(dt)
+		dt = dt.split("$")
+		del dt[len(dt)-1]
+		for i in dt:
+			x = i.split("#")
+			x[2] = x[2].replace("\n","")
+			dm = DataModule(x[0],x[1],x[2])
+			data.append(dm)
+def savePwFile():
+	s = open("pws.encr","wt")
+	b = ""
+	for i in data:
+		b += str(i.name+"#"+i.password+"#"+i.website)
+		b += "$"
+	b = encrypt(b).decode('utf-8')
+	s.write(b)
+def genPassword():
+	x = int(binascii.hexlify(os.urandom(8)).decode('utf-8'),16)
+	y = int(binascii.hexlify(os.urandom(5)).decode('utf-8'),16)
+	c = int(binascii.hexlify(os.urandom(2)).decode('utf-8'),16)
+	ps = []
+	pw = ""
+	i = 0
+	while x > len(CHARS):
+		i += 1
+		ps.append(x % len(CHARS))
+		x -= y
+		if i > 100000:
+			break
+	for i in range(PASSWORD_LENGHT):
+		pw += str(CHARS[ps[c]])
+	return pw
+def addNewEntryG(name,website):#Generate a new random Password for this website
+	data.append(DataModule(name,genPassword(),website))
+def addNewEntryM(name,password,website):#Use the user's password
+	data.append(DataModule(name,password,website))
+
+#Encryption Setup
+BLOCK_SIZE = 32
+PADDING = '{'
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+EncodeAES = lambda c , s: base64.b64encode(c.encrypt(pad(s)))
+DecodeAES = lambda c , e: c.decrypt(base64.b64decode(e)).decode('utf-8').rstrip(PADDING)
+
+secret = 
