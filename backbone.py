9bdf11b9e28c041a97801adaaf0578592a8d4e9b
timseinelps <tim11081999@gmail.com>
2015-11-19 22:30:43 +0100

    backbone

--- backbone.py
+++ backbone.py
@@ -1,4 +1,4 @@
-import random , os , math , binascii , base64 , getpass
+import random , os , math , binascii , base64 , getpass , hashlib
 from tkinter import Tk
 from Crypto.Cipher import AES
 
@@ -27,62 +27,66 @@ class DataModule:
 		self.website = website
 		self.password = password
 		self.name = name
-		
-def encrypt(data):
-	encoded = EncodeAES(cipher,data)
-	return encoded
-def decrypt(data):
-	decrypt = DecodeAES(cipher,data)
-	return decoded
+class Mngmnt:
+	def __init__(self,password):
+		self.secret = hashlib.md5(password.encode('utf-8')).hexdigest()
+		self.cipher = AES.new(self.secret)
+	def encrypt(data):
+		encoded = EncodeAES(cipher,data)
+		return encoded
+	def decrypt(data):
+		decrypt = DecodeAES(cipher,data)
+		return decoded
 	
-def loadPwFile():
-	if os.stat("pws.encr").st_size == 0:
-		return 0
-	else:
-		s = open("pws.encr","rt")
-		dt = s.readline()
-		dt = decrypt(dt)
-		dt = dt.split("$")
-		del dt[len(dt)-1]
-		for i in dt:
-			x = i.split("#")
-			x[2] = x[2].replace("\n","")
-			dm = DataModule(x[0],x[1],x[2])
-			data.append(dm)
-def savePwFile():
-	s = open("pws.encr","wt")
-	b = ""
-	for i in data:
-		b += str(i.name+"#"+i.password+"#"+i.website)
-		b += "$"
-	b = encrypt(b).decode('utf-8')
-	s.write(b)
-def genPassword():
-	x = int(binascii.hexlify(os.urandom(8)).decode('utf-8'),16)
-	y = int(binascii.hexlify(os.urandom(5)).decode('utf-8'),16)
-	c = int(binascii.hexlify(os.urandom(2)).decode('utf-8'),16)
-	ps = []
-	pw = ""
-	i = 0
-	while x > len(CHARS):
-		i += 1
-		ps.append(x % len(CHARS))
-		x -= y
-		if i > 100000:
-			break
-	for i in range(PASSWORD_LENGHT):
-		pw += str(CHARS[ps[c]])
-	return pw
-def addNewEntryG(name,website):#Generate a new random Password for this website
-	data.append(DataModule(name,genPassword(),website))
-def addNewEntryM(name,password,website):#Use the user's password
-	data.append(DataModule(name,password,website))
-
+	def loadPwFile():
+		if os.stat("pws.encr").st_size == 0:
+			return 0
+		else:
+			s = open("pws.encr","rt")
+			dt = s.readline()
+			dt = decrypt(dt)
+			dt = dt.split("$")
+			del dt[len(dt)-1]
+			for i in dt:
+				x = i.split("#")
+				x[2] = x[2].replace("\n","")
+				dm = DataModule(x[0],x[1],x[2])
+				data.append(dm)
+	def savePwFile():
+		s = open("pws.encr","wt")
+		b = ""
+		for i in data:
+			b += str(i.name+"#"+i.password+"#"+i.website)
+			b += "$"
+		b = encrypt(b).decode('utf-8')
+		s.write(b)
+	def genPassword():
+		x = int(binascii.hexlify(os.urandom(8)).decode('utf-8'),16)
+		y = int(binascii.hexlify(os.urandom(5)).decode('utf-8'),16)
+		c = int(binascii.hexlify(os.urandom(2)).decode('utf-8'),16)
+		ps = []
+		pw = ""
+		i = 0
+		while x > len(CHARS):
+			i += 1
+			ps.append(x % len(CHARS))
+			x -= y
+			if i > 100000:
+				break
+		for i in range(PASSWORD_LENGHT):
+			pw += str(CHARS[ps[c]])
+		return pw
+	def addNewEntryG(name,website):#Generate a new random Password for this website
+		data.append(DataModule(name,genPassword(),website))
+	def addNewEntryM(name,password,website):#Use the user's password
+		data.append(DataModule(name,password,website))		
 #Encryption Setup
 BLOCK_SIZE = 32
 PADDING = '{'
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
 EncodeAES = lambda c , s: base64.b64encode(c.encrypt(pad(s)))
 DecodeAES = lambda c , e: c.decrypt(base64.b64decode(e)).decode('utf-8').rstrip(PADDING)
-
-secret = 
+PASSWORD_LENGHT = 50#Safer Config random.randint(50,53) -> more password-lenghts to bruteforce | not needed
+CHARS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','.',',','?','*','&','"','+','-']
+	
+	
