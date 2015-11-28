43c41e15c90f0f10908ebfc078de8d22d9604710
timseinelps <tim11081999@gmail.com>
2015-11-28 16:49:08 +0100

    making shure all the code has been pushed to github

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ This is a password generation and encryption tool.
 This Software is provided as is without any warranty. In no case should the Author be liable for the Software.
 Tim Hartmann , November 2015
 """
-import random , os , math , binascii , base64 , getpass
+import random , os , binascii , base64 , getpass
 from tkinter import Tk
 from Crypto.Cipher import AES
 from backbone import switch , DataModule , Mngmnt
